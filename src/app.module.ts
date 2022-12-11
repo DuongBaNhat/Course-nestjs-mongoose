77715162541c2b57b109564c9b3c69adfe0d61f6
Duong Ba Nhat <banhatduong@gmailcom>
2022-12-11 16:45:24 +0700

    Other

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getConfig } from 'config/server';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PermissionModule } from './permission/permission.module';
+import { RolePermissionModule } from './role_permission/role_permission.module';
+import { RoleModule } from './role/role.module';
+import {  } from '@nestjs/core';
 
 @Module({
   imports: [
     MongooseModule.forRoot(getConfig().MONGO_URL),
     PermissionModule,
+    RolePermissionModule,
+    RoleModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+  ],
 })
 export class AppModule {}
