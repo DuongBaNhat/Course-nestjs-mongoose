875aaac7b14672c5c3b4871207c4b6724f219f1f
Duong Ba Nhat <banhatduong@gmailcom>
2022-12-13 12:09:55 +0700

    other

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,23 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { getConfig } from 'config/server';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PermissionModule } from './permission/permission.module';
-import { RoleModule } from './role/role.module';
+import { PermissionModule } from './modules/permission/permission.module';
+import { RoleModule } from './modules/role/role.module';
 import {  } from '@nestjs/core';
+import { LevelModule } from './modules/level/level.module';
+import { LessonModule } from './modules/lesson/lesson.module';
+import { CategoryModule } from './modules/category/category.module';
+import { CourseModule } from './modules/course/course.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(getConfig().MONGO_URL),
     PermissionModule,
     RoleModule,
+    LevelModule,
+    LessonModule,
+    CategoryModule,
+    CourseModule,
   ],
   controllers: [AppController],
   providers: [
